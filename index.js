4501c80aef0938cf3c4cf6a5789639ebf5523aa6
Giacomo Mariani <giacomo.mariani@aalto.fi>
2020-12-29 11:21:17 +0200

    service ready

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config()
+const app = express();
+
+const port = process.env.SERVICE_PORT;
+
+app.use(cors())
+app.get('/', (req,res) => {
+  res.end('Hello from service')
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
